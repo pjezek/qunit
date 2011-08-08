f46d35e74735ae703fa599d5e3069e438adf38c5
Patrick Jezek <patrick@jezek.ch>
2011-08-08 20:39:46 +0200

    added new option to pass a logfilename and results will be written in there (hackish: overloading console.log)

--- test.js
+++ test.js
@@ -15,18 +15,24 @@ var finished = false,
     pollResolution = 100,
     isPolling = false,
     page = new WebPage(),
+    logLines = "";
 /* Global helper functions */
     isFinished, onFinishedTests, pollForFinishedTests;
 
 // Print usage if called with wrong arguments count.
 if (phantom.args.length === 0 || phantom.args.length > 2) {
-    console.log('Usage: run-qunit.js URL');
+    console.log('Usage: test.js URL [LOGFILE]');
     phantom.exit(1);
+}
+
+// Catch "console.log" globally as text
+window.console.log = function (text) {
+    logLines += text;
 }
 
 // Catch random output as xml comment.
 console.log('<?xml version="1.0"?>\n');
-console.log("<!-- ");
+console.log("<!-- \n");
 console.log("executing: " + phantom.args[0] + "\n");
 
 // Read isFinished from logger and returns it.
@@ -42,18 +48,21 @@ function onFinishedTests () {
     var output = page.evaluate(function() {
         return QUnit.xmlWriter.getXML();
     });
-    console.log("-->");
+    console.log("-->\n");
     console.log(output);
     // TODO: write to disk
-    // try {
-    //     f = fs.open("./javascript.xml", "w");
-    //     f.write(QUnit.xmlWriter.xmlString);
-    //     f.close();
-    // } catch (e) {
-    //     console.log(e);
-    //     console.log("phantomjs> Unable to save result of Suite ''");
-    // }
-    phantom.exit(0);
+    if (2 === phantom.args.length && "undefined" !== typeof phantom.args[1]) {
+        try {
+            f = fs.open(phantom.args[1], "w");
+            f.write(logLines);
+            f.close();
+        } catch (e) {
+            console.log(e);
+            console.log("phantomjs> Unable to save result of Suite '" + phantom.args[1] + "'");
+            phantom.exit(1);
+        }
+        phantom.exit(0);
+    }
 };
 
 // Polls Logger until all tests are finished with execution.
